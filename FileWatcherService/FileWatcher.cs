cf8fee35d4da9ecc2a9f003e93eef122b42fb63a
BURAK ÖVEÇ <burakovecc@outlook.com>
2021-10-27 17:59:23 +0300

    Add project files.

---
+++ FileWatcherService/FileWatcher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace FileWatcherService
+{
+    class FileWatcher
+    {
+        Timer _timer = new Timer();
+
+        public void Start()
+        {
+            WriteToFile("Service starts at " + DateTime.Now);
+            _timer.Elapsed += new ElapsedEventHandler(onElapsedTime);
+            _timer.Enabled = true;
+            _timer.Interval = 1000;
+        }
+
+        private void onElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            WriteToFile("Service recalls at " + DateTime.Now);
+        }
+
+        public void Stop()
+        {
+            WriteToFile("Service stops at " + DateTime.Now);
+        }
+
+        private void WriteToFile(string message)
+        {
+            string folderPath = AppDomain.CurrentDomain.BaseDirectory + "ServiceLogPath";
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            string filePath = folderPath + @"\" + "logfile" + ".txt";
+
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter streamWriter = File.CreateText(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter streamWriter = File.AppendText(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                }
+            }
+        }
+
+    }
+}
---
+++ FileWatcherService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Topshelf;
+
+namespace FileWatcherService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hostConfig =>
+            {
+                hostConfig.Service<FileWatcher>(serviceConfig =>
+                {
+                    serviceConfig.ConstructUsing(fileWatcher => new FileWatcher());
+                    serviceConfig.WhenStarted(fileWatcher => fileWatcher.Start());
+                    serviceConfig.WhenStopped(fileWatcher => fileWatcher.Stop());
+                });
+                hostConfig.RunAsLocalSystem();
+                hostConfig.SetServiceName("FileWatcherProject");
+                hostConfig.SetDisplayName("File Watcher Project");
+            });            
+        }
+    }
+}
